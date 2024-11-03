b6acb457af7f694a269afcfb78d160d5d649ca1c
singh-abhinash <abhinashsingh2021po@gmail.com>
2024-11-03 23:47:00 +0530

    Implemented code for Contains Duplicate II

---
+++ src/main/java/hashmap/containsDuplicateII/Solution.java
@@ -0,0 +1,20 @@
+package hashmap.containsDuplicateII;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // stores number and its latest index
+        HashMap<Integer, Integer> numIndexMap = new HashMap<>(); 
+        for (int i = 0; i < nums.length; i++) {
+            if (numIndexMap.containsKey(nums[i])) {
+                if (i - numIndexMap.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            // update the index of the current number
+            numIndexMap.put(nums[i], i);
+        }
+        return false;
+    }
+}
