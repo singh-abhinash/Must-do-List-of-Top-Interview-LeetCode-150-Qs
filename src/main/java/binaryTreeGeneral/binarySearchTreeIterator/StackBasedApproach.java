739bfbb7c6d8a5d02bba99850aabce23e03adaab
singh-abhinash <abhinashsingh2021po@gmail.com>
2024-12-31 08:20:07 +0530

    added code for bst iterator

--- src/main/java/binaryTreeGeneral/binarySearchTreeIterator/StackBasedApproach.java
+++ src/main/java/binaryTreeGeneral/binarySearchTreeIterator/StackBasedApproach.java
@@ -36,9 +36,9 @@ public class StackBasedApproach {
 /**
  * next involves two major operations. One is where we pop an element from the stack which becomes the next smallest element 
  * to return. This is a O(1) operation. However, we then make a call to our helper function _inorder_left which iterates over
- *  a bunch of nodes. This is clearly a linear time operation i.e. O(N) in the worst case. This is true.
+ * a bunch of nodes. This is clearly a linear time operation i.e. O(N) in the worst case. This is true.
 
-However, the important thing to note here is that we only make such a call for nodes which have a right child. Otherwise, we 
+  However, the important thing to note here is that we only make such a call for nodes which have a right child. Otherwise, we 
 simply return. Also, even if we end up calling the helper function, it won't always process N nodes. 
 They will be much lesser. Only if we have a skewed tree would there be N nodes for the root. But that is the only node for 
 which we would call the helper function.
